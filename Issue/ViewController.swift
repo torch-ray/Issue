7425ab19913ffbd4081950ea6383c00a68379580
Ray suh <readingod@naver.com>
2021-09-05 08:06:48 +0900

    feat: 프로젝트 생성

---
+++ Issue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Issue
+//
+//  Created by 서우석 on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
