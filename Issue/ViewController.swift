b6831bf60276dce7772ef6d819d513f52137871f
Ray suh <readingod@naver.com>
2021-09-05 08:10:15 +0900

    chore: 불필요한 주석 및 메서드 삭제

--- Issue/ViewController.swift
+++ Issue/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Issue
-//
-//  Created by 서우석 on 2021/09/05.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
