499538c254838a301d759d4744c42973be549c6e
Ray suh <readingod@naver.com>
2021-09-05 08:50:37 +0900

    feat: UICollectionView 추가

--- Issue/View/Controller/MainViewController.swift
+++ Issue/View/Controller/MainViewController.swift
@@ -2,8 +2,31 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    private lazy var listCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout:layout)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setup()
+    }
+}
+
+// MARK: Setup
+private extension MainViewController {
+    private func setup() {
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        view.addSubview(listCollectionView)
+        listCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        listCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        listCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        listCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        listCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        listCollectionView.backgroundColor = .white
     }
 }
