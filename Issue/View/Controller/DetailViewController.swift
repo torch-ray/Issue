cfc255edec15e1e433bde4a5401cd10bbeb1e038
Ray suh <readingod@naver.com>
2021-09-05 11:17:02 +0900

    feat: DetailViewController 생성

---
+++ Issue/View/Controller/DetailViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class DetailViewController: UIViewController, ViewModelBindableType {
+    
+    var viewModel: DetailViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func bindViewModel() {
+    }
+}
