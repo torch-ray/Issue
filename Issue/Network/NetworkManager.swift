73c037f44c7369b75443ed08fc2bf645aee9727d
Ray suh <readingod@naver.com>
2021-09-05 10:49:19 +0900

    feat: 네트워크 호출 메서드 작성

--- Issue/Network/NetworkManager.swift
+++ Issue/Network/NetworkManager.swift
@@ -1,5 +1,23 @@
 import Foundation
+import Alamofire
+import RxSwift
 
 final class NetworkManager: NetworkManagerType {
     
+    func getIssue(_ url: URL) -> Observable<Issue> {
+        return Observable.create { observer in
+            AF.request(url, method: .get)
+                .responseDecodable(of: Issue.self) { response in
+                    switch response.result {
+                    case .failure(let error):
+                        observer.onError(error)
+                    case .success(let data):
+                        observer.onNext(data)
+                        observer.onCompleted()
+                    }
+                }
+            return Disposables.create()
+        }
+        
+    }
 }
--- Issue/Network/NetworkManagerType.swift
+++ Issue/Network/NetworkManagerType.swift
@@ -1,4 +1,7 @@
 import Foundation
+import RxSwift
 
 protocol NetworkManagerType {
+    
+    func getIssue(_ url: URL) -> Observable<Issue>
 }
