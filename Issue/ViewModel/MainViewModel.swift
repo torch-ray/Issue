a7b60b4c3ec474a6dea4102bba7ff2f44e216685
Ray suh <readingod@naver.com>
2021-09-05 10:15:29 +0900

    feat: 스토리지의 이슈 정보 메인 뷰모델로 가져오기

--- Issue/ViewModel/MainViewModel.swift
+++ Issue/ViewModel/MainViewModel.swift
@@ -1,5 +1,9 @@
 import Foundation
+import RxCocoa
 
 final class MainViewModel: CommonViewModel {
     
+    lazy var issuList: Driver<[Issue]> = {
+        return storage.list().asDriver(onErrorJustReturn: [])
+    }()
 }
