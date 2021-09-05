02a2a1f1e0102b085869c40f5e9bbbcc8dc035b6
Ray suh <readingod@naver.com>
2021-09-05 13:34:38 +0900

    feat: 검색어 입력할 텍스트 필드 띄우기

--- Issue/View/Controller/SearchViewController.swift
+++ Issue/View/Controller/SearchViewController.swift
@@ -9,6 +9,12 @@ final class SearchViewController: UIViewController, ViewModelBindableType {
         return view
     }()
     
+    
+    private lazy var repositorySearchTextField: UITextField = {
+        let searchTextField = UITextField()
+        return searchTextField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -23,6 +29,7 @@ private extension SearchViewController {
     private func setup() {
         view.isOpaque = true
         setupMainView()
+        setupTextField()
     }
     
     private func setupMainView() {
@@ -30,8 +37,19 @@ private extension SearchViewController {
         mainView.backgroundColor = .white
         mainView.translatesAutoresizingMaskIntoConstraints = false
         mainView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
-        mainView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        mainView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
         mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setupTextField() {
+        mainView.addSubview(repositorySearchTextField)
+        repositorySearchTextField.borderStyle = .line
+        repositorySearchTextField.placeholder = "검색어를 입력하세요."
+        repositorySearchTextField.translatesAutoresizingMaskIntoConstraints = false
+        repositorySearchTextField.widthAnchor.constraint(equalTo: mainView.widthAnchor, multiplier: 0.8).isActive = true
+        repositorySearchTextField.heightAnchor.constraint(equalTo: mainView.heightAnchor, multiplier: 0.2).isActive = true
+        repositorySearchTextField.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
+        repositorySearchTextField.centerYAnchor.constraint(equalTo: mainView.centerYAnchor).isActive = true
     }
 }
