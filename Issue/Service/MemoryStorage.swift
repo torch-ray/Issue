a0974985416e00abe35da5040fa31439ef0182b9
Ray suh <readingod@naver.com>
2021-09-05 10:12:34 +0900

    feat: 생성,조회 메서드 구현

--- Issue/Service/MemoryStorage.swift
+++ Issue/Service/MemoryStorage.swift
@@ -2,4 +2,11 @@ import Foundation
 
 final class MemoryStrorage: MemoryStorageType {
     
+    @discardableResult
+    func create(_ issue: Issue) -> Observable<Issue> {
+    }
+    
+    @discardableResult
+    func list() -> Observable<[Issue]> {
+    }
 }
--- Issue/Service/MemoryStorageType.swift
+++ Issue/Service/MemoryStorageType.swift
@@ -1,5 +1,11 @@
 import Foundation
+import RxSwift
 
 protocol MemoryStorageType {
     
+    @discardableResult
+    func create(_ issue: Issue) -> Observable<Issue>
+    
+    @discardableResult
+    func list() -> Observable<[Issue]>
 }
