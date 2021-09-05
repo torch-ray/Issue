fa9ef1aa5a3c710be3535fcb7cf7181e50155ada
Ray suh <readingod@naver.com>
2021-09-05 10:14:11 +0900

    feat: 생성, 조회 메서드 디테일 구현

--- Issue/Service/MemoryStorage.swift
+++ Issue/Service/MemoryStorage.swift
@@ -1,12 +1,20 @@
 import Foundation
+import RxSwift
 
 final class MemoryStrorage: MemoryStorageType {
     
+    private var issueStorage: [Issue] = []
+    private lazy var store = BehaviorSubject<[Issue]>(value: issueStorage)
+    
     @discardableResult
     func create(_ issue: Issue) -> Observable<Issue> {
+        issueStorage.append(issue)
+        store.onNext(issueStorage)
+        return Observable.just(issue)
     }
     
     @discardableResult
     func list() -> Observable<[Issue]> {
+        return store
     }
 }
